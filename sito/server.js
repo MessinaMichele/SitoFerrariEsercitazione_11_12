19ce11104ce19a427beaacc9f887db58de0334e7
AnonimoMC <messina.michele@iisgalvanimi.edu.it>
2023-12-21 08:16:52 +0000

    Esercizio_11_12

--- sito/server.js
+++ sito/server.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
-const port = process.env.PORT || '4200';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 app.listen(port, () => {
